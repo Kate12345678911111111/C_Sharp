fc969ab4f519e68cc16a1edd4261a9cc7b196f23
Kate_Volobueva <kate.50@mail.ru>
2022-08-13 23:19:07 +0300

    zadachi 19,21,23

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -3,11 +3,54 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+
+
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// Console.Clear();
+
+// Console.Write("введите x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("введите x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("введите x3: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите y3: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+
+// double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+Math.Pow(y - y2, 2);
+
+// Console.WriteLine($"d={d:f2}");
+
+
+
+
+
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+
+// Console.Clear();
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine());
+// int N1 = Math.Abs(N);
+// for(int i=1; i<=N1; i++)
+// {
+//     Console.Write($"{i*i*i} ");
+
+// }
+
