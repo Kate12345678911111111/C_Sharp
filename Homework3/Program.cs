cdfca1cd03791c5a15135425101c82058c0f2df9
Kate_Volobueva <kate.50@mail.ru>
2022-08-14 12:06:48 +0300

    zadachi 19,21,23

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -6,6 +6,13 @@
 
 
 
+
+
+
+
+
+
+
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
@@ -18,23 +25,20 @@
 // int x1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("введите y1: ");
 // int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
 
 // Console.Write("введите x2: ");
 // int x2 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("введите y2: ");
 // int y2 = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("введите x3: ");
-// int x1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("введите y3: ");
-// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
 
-// double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+Math.Pow(y - y2, 2);
+// double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)+Math.Pow(z1 - z2, 2));
 
-// Console.WriteLine($"d={d:f2}");
-
-
+// Console.WriteLine($"d={d:f5}");
 
 
 
