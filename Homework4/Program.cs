e53336416cac9de40f5ed23621ddb0dd53a6e24e
Kate_Volobueva <kate.50@mail.ru>
2022-08-20 16:37:03 +0300

    sem5

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -146,3 +146,73 @@
 
 //     return result;
 // }
+
+
+// Задайте массив из 12 элементов, заполненный случайными числами от -9 до 9. Посчитать сумму положительных чисел и сумму отрицательных чисел
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] Katearray = CreateRandomArray(12, -9, 9);
+// ShowArray(Katearray);
+// Console.WriteLine("---------------------------------");
+
+// int sum_pos = 0;
+// int sum_neg = 0;
+// for (int i = 0; i < Katearray.Length; i++)
+// {
+//     if (Katearray[i] > 0)
+//         sum_pos += Katearray[i];
+//     else
+//         sum_neg += Katearray[i];
+// }
+// Console.WriteLine($"Сумма положительных: {sum_pos}. Сумма отрицательных: {sum_neg}");
+
+
+//  Напиши программу замена элементов массива: положительные элементы замените на отрицательные и наоборот
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+    
+// }
+
+//     int[] Newarray = CreateRandomArray(12, -9, 9);
+//     ShowArray(Newarray);
+
+// for (int i = 0; i < Newarray.Length; i++)
+// {
+//     Newarray[i] *=  -1;
+// }
+// ShowArray(Newarray);
+
