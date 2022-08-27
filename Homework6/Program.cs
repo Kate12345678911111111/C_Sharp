337695cbbf434d0c7b9c5ad8f2b613aef443cbe1
Kate_Volobueva <kate.50@mail.ru>
2022-08-27 16:03:44 +0300

    Zadachi 41, 43

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -2,33 +2,33 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// double[] CreateRandomArray(double M, double start, double end)
+// int[] CreateRandomArray(int M, int start, int end)
 // {
-//     double[] RandomArray = new double[M];
-//     for (double i = 0; i < M; i++)
+//     int[] RandomArray = new int[M];
+//     for (int i = 0; i < M; i++)
 //     {
 //         RandomArray[i] = new Random().Next(start, end + 1);
 //     }
 //     return RandomArray;
 // }
 
-// void ShowArray(double[] array)
+// void ShowArray(int[] array)
 // {
-//     for (double i = 0; i < array.Length; i++)
+//     for (int i = 0; i < array.Length; i++)
 //     {
 //         Console.Write(array[i] + " ");
 //     }
 //     Console.WriteLine();
 // }
 // Console.Clear();
-// double M = new Random().Next(1, 10);
+// int M = new Random().Next(1, 10);
 // Console.WriteLine("Длинна массива: " + M);
-// double[] Katearray = CreateRandomArray(M, -10, 10);
+// int[] Katearray = CreateRandomArray(M, -10, 10);
 // Console.WriteLine($"{String.Join(", ",Katearray)}");
 
 
-// double count = 0;
-// for (double i = 0; i < Katearray.Length; i++)
+// int count = 0;
+// for (int i = 0; i < Katearray.Length; i++)
 // {
 //     if (Katearray[i] > 0)
 //         count++;
@@ -42,31 +42,31 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-Console.Clear();
+// Console.Clear();
 
-Console.Write("введите k1: ");
-double k1 = double.Parse(Console.ReadLine());
-Console.Write("введите b1: ");
-double b1 = double.Parse(Console.ReadLine());
-Console.Write("введите k2: ");
-double k2 = double.Parse(Console.ReadLine());
-Console.Write("введите b2: ");
-double b2 = double.Parse(Console.ReadLine());
+// Console.Write("введите k1: ");
+// int k1 = int.Parse(Console.ReadLine());
+// Console.Write("введите b1: ");
+// int b1 = int.Parse(Console.ReadLine());
+// Console.Write("введите k2: ");
+// int k2 = int.Parse(Console.ReadLine());
+// Console.Write("введите b2: ");
+// int b2 = int.Parse(Console.ReadLine());
 
-double x = 0;
-double y = 0;
+// int x = 0;
+// int y = 0;
 
-if (k1 != k2)
-{ 
-x = (b2 - b1) / (k1 - k2);
-y = k1 * x + b1;
-Console.WriteLine($"x={x:f1}, y={y:f1}");
-}
+// if (k1 != k2)
+// { 
+// x = (b2 - b1) / (k1 - k2);
+// y = k1 * x + b1;
+// Console.WriteLine($"x={x:f1}, y={y:f1}");
+// }
 
-else
-{
-    Console.WriteLine("Прямые параллельны и пересекаться не могут");
-}
+// else
+// {
+//     Console.WriteLine("Прямые параллельны и пересекаться не могут");
+// }
 
 
 
